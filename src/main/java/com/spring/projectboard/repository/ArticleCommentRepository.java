2367faf38afdab0d99ba1584f4eec7f6020a40c8
Jaeha Lee <wogkdkrm112@gmail.com>
2024-05-11 10:07:05 +0900

    feat: Querydsl

--- src/main/java/com/spring/projectboard/repository/ArticleCommentRepository.java
+++ src/main/java/com/spring/projectboard/repository/ArticleCommentRepository.java
@@ -2,7 +2,9 @@ package com.spring.projectboard.repository;
 
 import com.spring.projectboard.domain.ArticleComment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
 
 
--- src/main/java/com/spring/projectboard/repository/ArticleRepository.java
+++ src/main/java/com/spring/projectboard/repository/ArticleRepository.java
@@ -2,8 +2,13 @@ package com.spring.projectboard.repository;
 
 import com.spring.projectboard.domain.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-public interface ArticleRepository extends JpaRepository<Article, Long> {
-
-
+@RepositoryRestResource
+public interface ArticleRepository extends
+        JpaRepository<Article, Long>,
+        QuerydslPredicateExecutor<Article>
+{
 }
