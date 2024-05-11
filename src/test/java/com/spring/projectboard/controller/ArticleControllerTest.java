b617cd398a984a59d39706f0535015daba71dc2b
Jaeha Lee <wogkdkrm112@gmail.com>
2024-05-11 22:06:03 +0900

    feat: ArticleController, ArticleController test 추가중

---
+++ src/test/java/com/spring/projectboard/controller/ArticleControllerTest.java
@@ -0,0 +1,26 @@
+package com.spring.projectboard.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@DisplayName("View 컨트롤러 - 게시글")
+@WebMvcTest
+class ArticleControllerTest {
+
+    private final MockMvc mvc;
+
+    public ArticleControllerTest(@Autowired MockMvc mvc) {
+        this.mvc = mvc;
+    }
+
+
+    @DisplayName("[view][GET] 게시글 리스트 (게시판) 페이지 - 정상 호출")
+    @Test
+    public void givenNothing_whenRequestingArticlesView_thenReturnsArticlesView() {
+
+        mvc.perform(get())
+    }
+}
